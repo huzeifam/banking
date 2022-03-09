bb73f15341aead006424a652ad752bfb7b86b829
huzeifam <huzeifa.marzouka@adesso.de>
2022-03-09 15:59:06 +0100

    Hello World

--- src/main/java/com/example/banking/controller/HelloWorldController.java
+++ src/main/java/com/example/banking/controller/HelloWorldController.java
@@ -1,4 +1,15 @@
 package com.example.banking.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+
+    @GetMapping("/hallo")
+    public String hallo(){
+        return "Hello World";
+    }
+
 }
